21a3d91c2edef72d6f7698ce07f523ccd5abab6b
Caleb Flores <cflores@applaudostudios.com>
2020-11-11 19:42:07 -0600

    fix issue with multiples requests

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,17 +1,7 @@
-import React,{useState,useEffect} from 'react';
+import React from 'react';
 import axios from 'axios';
 
-export default ({postId}) => {
-    const [comments,setComments] = useState([]);
-
-    const fetchComments = async () => {
-        const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`);
-        setComments(res.data);
-    };
-
-    useEffect(()=>{
-        fetchComments();
-    },[])
+export default ({comments}) => {
 
     const renderedComments = comments.map((comment) => {
         return <li key={comment.id}>{comment.content}</li>
