03d0de8977d1edbddf5598722974d7802adffd71
Caleb Flores <cflores@applaudostudios.com>
2020-11-17 17:52:32 -0600

    add ingress service

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
+const cors = require('cors');
 
 var app = express();
-
+app.use(cors());
 app.use(bodyParser.json());
 
 const events = [];
