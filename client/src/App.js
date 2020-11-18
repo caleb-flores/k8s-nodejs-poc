5316ae5c472310f2dac7a18abab120659cd92be4
Caleb Flores <cflores@applaudostudios.com>
2020-11-17 20:25:45 -0600

    add skaffold support

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import PostCreate  from "./PostCreate";
 import PostList from "./PostList";
 export default () => {
     return <div className="container">
-        <h1>Create Post</h1>
+        <h1>Create Post!!!!!!</h1>
         <PostCreate />
         <hr/>
         <PostList/>
